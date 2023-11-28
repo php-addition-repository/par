47acf25ec7bab866c56bf4df9a1442f3239d6c07
Alex Brouwer <alex@factorit.nl>
2023-11-28 16:51:01 +0100

    Added docBlock and allow mixed via psalm

--- packages/core/src/Values.php
+++ packages/core/src/Values.php
@@ -8,6 +8,21 @@ use DateTimeInterface;
 
 final class Values
 {
+    /**
+     * Determines if values should be considered equal.
+     *
+     * - If `$value` implements `Par\Core\ObjectEquality` then `$value->equals($otherValue)` is used.
+     * - If `$otherValue` implements `Par\Core\ObjectEquality` then `$otherValue->equals($value)` is used.
+     * - When both values implement the `DateTimeInterface` they are considered equal when the equal when formatted
+     *   using `RFC3339_EXTENDED`.
+     * - Otherwise a strict comparison (`$value === $otherValue`) is used.
+     *
+     * @param mixed $value A value
+     * @param mixed $otherValue The other value with which to compare
+     *
+     * @return bool True if both values should be considered equal
+     * @psalm-mutation-free
+     */
     public static function equals(mixed $value, mixed $otherValue): bool
     {
         if ($value instanceof ObjectEquality) {
@@ -29,10 +44,14 @@ final class Values
      * @param DateTimeInterface $value
      * @param DateTimeInterface $otherValue
      * @return bool
+     *
+     * @psalm-mutation-free
+     * @psalm-suppress ImpureMethodCall
      */
     private static function compareDateTimes(DateTimeInterface $value, DateTimeInterface $otherValue): bool
     {
         $comparisonFormat = DateTimeInterface::RFC3339_EXTENDED;
+
         return $value->format($comparisonFormat) === $otherValue->format($comparisonFormat);
     }
 }
