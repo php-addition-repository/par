7e49fa169a68359cb424a0db09879c3322dc4646
Alex Brouwer <alex@factorit.nl>
2023-11-29 10:04:26 +0100

    Add testcase with failing assertion

---
+++ packages/core/test/Unit/FailingTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Par\CoreTest\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class FailingTest extends TestCase
+{
+    public function testItReportsFailure()
+    {
+        $this->assertFalse(true);
+    }
+}
