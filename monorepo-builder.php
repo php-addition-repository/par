a8aad397baa06a6353fef7527e0e0fab70a496ff
Alex Brouwer <alex@factorit.nl>
2023-12-01 11:55:32 +0100

    [dx] Updated monorepo builder process to update the CHANGELOG

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+require_once "vendor/autoload.php";
+
+use App\MonorepoBuilder\Release\UpdateChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 use Symplify\MonorepoBuilder\Config\MBConfig;
-use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
@@ -27,10 +29,11 @@ return static function (MBConfig $mbConfig): void {
                 ],
             ],
             ComposerJsonSection::REQUIRE_DEV => [
-                "roave/security-advisories" => "dev-master",
                 "phpunit/phpunit" => "^10.4",
                 "psalm/plugin-phpunit" => "^0.18",
+                "roave/security-advisories" => "dev-master",
                 "squizlabs/php_codesniffer" => "^3.7",
+                "symfony/string" => "^6.4",
                 "symplify/monorepo-builder" => "^11.2",
                 "vimeo/psalm" => "^5.16"
             ],
@@ -40,7 +43,7 @@ return static function (MBConfig $mbConfig): void {
     $mbConfig->workers([
         UpdateReplaceReleaseWorker::class,
         SetCurrentMutualDependenciesReleaseWorker::class,
-        AddTagToChangelogReleaseWorker::class,
+        UpdateChangelogReleaseWorker::class,
         TagVersionReleaseWorker::class,
         PushTagReleaseWorker::class,
         SetNextMutualDependenciesReleaseWorker::class,
