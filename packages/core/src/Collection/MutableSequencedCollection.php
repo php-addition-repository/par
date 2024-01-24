f9ca10a64178405e3953a2efcd90a09f1eb76e55
Alex Brouwer <alex@factorit.nl>
2024-01-24 14:32:34 +0100

    feat(core): Added interface for a mutable sequenced collection

---
+++ packages/core/src/Collection/MutableSequencedCollection.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Par\Core\Collection;
+
+use Par\Core\Exception\NoSuchElementException;
+
+/**
+ * A mutable sequenced collection.
+ *
+ * @see SequencedCollection
+ * @template TValue
+ * @extends SequencedCollection<TValue>
+ * @extends MutableCollection<int<0, max>, TValue>
+ */
+interface MutableSequencedCollection extends SequencedCollection, MutableCollection
+{
+    /**
+     * Adds an element as the first element of this collection.
+     *
+     * @param TValue $element the element to be added
+     *
+     * @return void
+     */
+    public function addFirst(mixed $element): void;
+
+    /**
+     * Adds an element as the last element of this collection.
+     *
+     * @param TValue $element the element to be added
+     *
+     * @return void
+     */
+    public function addLast(mixed $element): void;
+
+    /**
+     * Removes and returns the first element of this sequence.
+     *
+     * @return TValue the removed element
+     * @throws NoSuchElementException if this sequence is empty
+     */
+    public function removeFirst(): mixed;
+
+    /**
+     * Removes and returns the last element of this sequence.
+     *
+     * @return TValue the removed element
+     * @throws NoSuchElementException if this sequence is empty
+     */
+    public function removeLast(): mixed;
+}
