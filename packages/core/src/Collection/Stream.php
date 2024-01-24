eb10f23ef605d4d80a0a70b41a41c0bdaaeece5c
Alex Brouwer <alex@factorit.nl>
2024-01-24 14:57:01 +0100

    Add class description

--- packages/core/src/Collection/Stream.php
+++ packages/core/src/Collection/Stream.php
@@ -19,6 +19,15 @@ use Par\Core\Optional;
 use Traversable;
 
 /**
+ * A sequence of elements supporting sequential operations.
+ *
+ * To perform a computation, stream operations are composed into a stream pipeline. A stream pipeline consists of a
+ * source (which might be an array, a collection, a generator function, etc), zero or more intermediate
+ * operations (which transform a stream into another stream, such as `filter(callable $predicate)`), and a terminal
+ * operation (which produces a result or side-effect, such as `count()` or `forEach(callable $action)`). Streams are
+ * lazy; computation on the source data is only performed when the terminal operation is initiated, and source elements
+ * are consumed only as needed.
+ *
  * @immutable
  * @template TValue
  * @implements IteratorAggregate<int, TValue>
